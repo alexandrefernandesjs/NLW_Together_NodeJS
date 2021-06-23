5acef2e2e90fde08fda8e7fcbb113063299fb60a
alexandrefernandesjs <alexandrefernandesjs@gmail.com>
2021-06-23 09:14:10 -0300

    --

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import "reflect-metadata";
+import { router } from "./routes";
+import "./database";
+
+// @types/express
+const app = express();
+app.use(express.json());
+app.use(router);
+
+// Tratando erros
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
+
+app.listen(3000, () => console.log('Server is running at port 3000!'));
